c7bd2dcc91b9139226b94f9f5a64030693d31d40
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-08-20 11:33:45 +0500

    Update outputs.tf

--- modules/app_stack/outputs.tf
+++ modules/app_stack/outputs.tf
@@ -14,3 +14,8 @@ output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.cdn.domain_name
   description = "Domain name of the CloudFront distribution"
 }
+
+output "acm_certificate_validation_options" {
+  value       = aws_acm_certificate.frontend_cert.domain_validation_options
+  description = "DNS records needed to validate the ACM certificate"
+}
